3e39293e02c4a08c36c6dc9223d115365ffae884
VanshSharma14 <vansh.business14@gmail.com>
2022-07-11 15:34:46 -0400

    added legbot command

---
+++ commands/letbot.js
@@ -0,0 +1,30 @@
+const OpenAI = require('openai-api')
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY
+const openai = new OpenAI(OPENAI_API_KEY);
+
+module.exports = {
+    name: "legbot",
+    usage: "legbot <your query>",
+    description: "Piinto uses the OpenAI chatbot API to answer and querys the user may have.",
+    action: async (msg, args) => {
+        args = args.join(" ");
+        (async () => {
+            const gptResponse = await openai.complete({
+                engine: 'text-davinci-002',
+                prompt: args,
+                maxTokens: 450,
+                temperature: 0.8,
+                topP: .3,
+                presencePenalty: 0,
+                frequencyPenalty: 0.5,
+                // bestOf: 1,
+                // n: 1,
+                // stream: false,
+                // stop: ['\n', '\n\n']
+        });
+        let rep = gptResponse.data.choices[0].text;
+        msg.reply(rep);
+        })();
+    }
+}
