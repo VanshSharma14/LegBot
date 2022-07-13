4b661393edf8eb95ac2e355c6cacd9139234aaef
VanshSharma14 <vansh.business14@gmail.com>
2022-07-13 12:00:09 -0400

    8 keys memory

--- commands/legbot.js
+++ commands/legbot.js
@@ -2,17 +2,26 @@ const OpenAI = require('openai-api')
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY
 const openai = new OpenAI(OPENAI_API_KEY);
-
+let arr = [];
 module.exports = {
     name: "legbot",
     usage: "legbot <your query>",
     description: "Piinto uses the OpenAI chatbot API to answer and querys the user may have.",
+    
     action: async (msg, args) => {
         args = args.join(" ");
+        args = (`${msg.author}: ${args}`)
+        if (arr.length == 8) {
+            arr.shift();
+            arr.push(args + "\n");
+        }
+        else{arr.push(args+ "\n")}
+        message = arr.join("")
+
         (async () => {
             const gptResponse = await openai.complete({
                 engine: 'text-davinci-002',
-                prompt: args,
+                prompt: message,
                 maxTokens: 450,
                 temperature: 0.8,
                 topP: .3,
