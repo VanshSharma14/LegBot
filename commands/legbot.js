11fca94eef626da44d0a8fe57319116747b21f52
VanshSharma14 <vansh.business14@gmail.com>
2022-07-14 15:37:26 +0000

    fixed legbot command back to normal

--- commands/legbot.js
+++ commands/legbot.js
@@ -2,7 +2,7 @@ const OpenAI = require('openai-api')
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY
 const openai = new OpenAI(OPENAI_API_KEY);
-let arr = [];
+// let arr = [];
 module.exports = {
     name: "legbot",
     usage: "legbot <your query>",
@@ -10,38 +10,48 @@ module.exports = {
     
     action: async (msg, args) => {
         args = args.join(" ");
-        args = `${msg.author}: ${args}`
+        // args = `${msg.author}: ${args}`
         // Piinto: motherfucker
 
-        if (arr.length == 8) {
-            arr.shift();
-            arr.shift()
-            
-        }
-        arr.push(args + "\n");
-        let message = arr.join("");
+        // save up to the last 8 messages
+        // if (arr.length == 8) {
+        //     arr.shift();
+        //     arr.shift()
+        // }
+        // arr.push(args + "\n");
+        // let message = arr.join("") + "LegBot: ";
 
         (async () => {
             const gptResponse = await openai.complete({
                 engine: 'text-davinci-002',
-                prompt: message,
+                prompt: args,
                 maxTokens: 450,
                 temperature: 0.8,
                 topP: .3,
-                presencePenalty: 0,
-                frequencyPenalty: 0.5,
+                presencePenalty: 1,
+                frequencyPenalty: 0.8,
                 // bestOf: 1,
                 // n: 1,
                 // stream: false,
                 // stop: ['\n', '\n\n']
-        });
-        let rep = gptResponse.data.choices[0].text;
-        rep = "!piinto " + rep;
+            });
+            let rep = gptResponse.data.choices[0].text;
+            // rep = handleReply(rep);
+            // arr.push(`LegBot: ${rep}\n`);
+            
+
+        // rep = "!piinto " + rep;
         if (rep.length > 2000){
             rep = rep.substring(0, 2000)
         }
-        arr.push(`LegBot: ${rep}`);
+        // console.log(arr)
         msg.reply(rep);
         })();
     }
 }
+
+function handleReply(reply) {
+    let x = reply.replace(/^\n*[lL]eg[bB]ot:\s*|^\n+/, '');
+    let y = x.replace(/^\n*[pP]iinto:\s*|^\n+/, '');
+    return y;
+  }
