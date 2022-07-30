9641954db2b936dfa8b01bd80eea7d88816e0665
VanshSharma14 <vansh.business14@gmail.com>
2022-07-30 16:35:55 -0400

    fixed replies

--- commands/grab.js
+++ commands/grab.js
@@ -1,62 +1,64 @@
-const { ApplicationCommandOptionChannelTypesMixin } = require("@discordjs/builders")
-const grabber = require("../models/grabber.js")
+const {
+  ApplicationCommandOptionChannelTypesMixin,
+} = require("@discordjs/builders");
+const grabber = require("../models/grabber.js");
 
 module.exports = {
-    name: "grab",
-    usage: "grab <@user-optional>",
-    description: "choose between one or more options",
-    action: async (msg, args) => {
-        let arglen = args.length
-        let contents = args[1]
-        let type = args[0]
-        
-        if (arglen != 2){
-            msg.reply("Usage: --grab <user/search> <@user/text>")
-            return;
-        }
-        await msg.channel.messages.fetch({limit: 20}).then(messages => {
-            if (messages.size == 20) {
-                const arr = Array.from(messages.values());
-                for(let i = 1; i < arr.length; i++) {
-                    // make sure it doesn't save a command
-                    if(arr[i].content.includes("--")){
-                         continue;
-                    }
-                    // msg.reply(`<@${msg.author.id}>`)
-                    let text = args[1].toLowerCase
-                    console.log(arr[i].contents)
-                    const lowQuote = arr[i].contents
-                    username = `<@${arr[i].author.id}>`
-                    console.log("Type: " + arr[i].content)
-                    console.log("username: " + username + " " + contents)
-                    if ((type == "user" && username == contents) || (type == "search" && arr[i].content.includes(args[1]))){
+  name: "grab",
+  usage: "grab <@user-optional>",
+  description: "choose between one or more options",
+  action: async (msg, args) => {
+    let arglen = args.length;
+    let contents = args[1];
+    let type = args[0];
 
-                        console.log("before")
-                        console.log(`arr[i] ${arr[i].content}`)
-                        try {
-                            const dbMessage = grabber.create({ 
-                                user: username,
-                                quote: arr[i].content
-                             })
-                        } catch (e) {
-                            console.log(e)
-                        }
-                        console.log("message saved")
-                        // find saved message
-                        grabber.find({'user': username}, 'quote', function (err, quotes)
-                        {
-                            if (err) return handleError(err)
-                        })
-
-                        msg.reply(`Added quote "${arr[i]}"`)
-                        return;
-                    }
-                    
-                }
-            }
-        })
+    if (arglen != 2) {
+      msg.reply("Usage: --grab <user/search> <@user/text>");
+      return;
     }
-}
+    await msg.channel.messages.fetch({ limit: 20 }).then((messages) => {
+      if (messages.size == 20) {
+        const arr = Array.from(messages.values());
+        for (let i = 1; i < arr.length; i++) {
+          // make sure it doesn't save a command
+          if (arr[i].content.includes("--")) {
+            continue;
+          }
+          // msg.reply(`<@${msg.author.id}>`)
+          let text = args[1].toLowerCase;
+          console.log(arr[i].contents);
+          const lowQuote = arr[i].contents;
+          username = `<@${arr[i].author.id}>`;
+          console.log("Type: " + arr[i].content);
+          console.log("username: " + username + " " + contents);
+          if (
+            (type == "user" && username == contents) ||
+            (type == "search" && arr[i].content.includes(args[1]))
+          ) {
+            console.log("before");
+            console.log(`arr[i] ${arr[i].content}`);
+            try {
+              const dbMessage = grabber.create({
+                user: username,
+                quote: arr[i].content,
+              });
+            } catch (e) {
+              console.log(e);
+            }
+            console.log("message saved");
+            // find saved message
+            grabber.find({ user: username }, "quote", function (err, quotes) {
+              if (err) return handleError(err);
+            });
+
+            msg.channel.send(`Added quote "${arr[i]}"`);
+            return;
+          }
+        }
+      }
+    });
+  },
+};
 
 // obj => gm {
 //     userID: "<@23423490823490>"
--- leg_bot.js
+++ leg_bot.js
@@ -1,68 +1,67 @@
-const Discord = require("discord.js")
-const mongoose = require("mongoose")
-const MongoClient = require('mongodb').MongoClient
-const { getCommands } = require("./util")
+const Discord = require("discord.js");
+const mongoose = require("mongoose");
+const MongoClient = require("mongodb").MongoClient;
+const { getCommands } = require("./util");
 const { Player, QueryType } = require("discord-player");
-require("dotenv").config()
+require("dotenv").config();
 // declare intent
-const intents = new Discord.Intents()
-intents.add("GUILDS")
-intents.add("DIRECT_MESSAGES")
-intents.add("GUILD_MESSAGES")
-intents.add("GUILD_MESSAGE_REACTIONS")
-intents.add("DIRECT_MESSAGE_REACTIONS")
-intents.add("GUILD_MEMBERS")
-intents.add("GUILD_BANS")
-intents.add("GUILD_VOICE_STATES")
+const intents = new Discord.Intents();
+intents.add("GUILDS");
+intents.add("DIRECT_MESSAGES");
+intents.add("GUILD_MESSAGES");
+intents.add("GUILD_MESSAGE_REACTIONS");
+intents.add("DIRECT_MESSAGE_REACTIONS");
+intents.add("GUILD_MEMBERS");
+intents.add("GUILD_BANS");
+intents.add("GUILD_VOICE_STATES");
 
-const client = new Discord.Client({intents: intents})
-const PREFIX = "--"
+const client = new Discord.Client({ intents: intents });
+const PREFIX = "--";
 exports.PREFIX = PREFIX;
-let dbName = 'grab'
+let dbName = "grab";
 client.on("ready", async () => {
-    await mongoose.connect(
-        process.env.MONGODB_URI,
-        {
-            keepAlive: true,
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            dbName: dbName
-        }
-    ).then(() => {
-        console.log("MONGOOSE ON TEH CASE")
-    }).catch((err) => {
-        console.log(err.message)
+  await mongoose
+    .connect(process.env.MONGODB_URI, {
+      keepAlive: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: dbName,
     })
-    console.log(`Logged in as ${client.user.tag}!`)
-}) 
+    .then(() => {
+      console.log("MONGOOSE ON TEH CASE");
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
 const commands = getCommands();
 
-client.on("messageCreate", async msg => {
-    console.log(msg.content)
-    // if (msg.author.bot) {
-    //     return;
-    // }
-    if (msg.content.startsWith(PREFIX)) {
-        let [cmdName, ...args] = msg.content.trim().split(/ +/);
-        cmdName = cmdName.slice(PREFIX.length).toLowerCase();
-        let cmdObj = commands[cmdName]
-        console.log(cmdName);
-        if (cmdObj){
-            cmdObj.action(msg, args)
-            console.log("sendingCommand")
-        }
+client.on("messageCreate", async (msg) => {
+  console.log(msg.content);
+  // if (msg.author.bot) {
+  //     return;
+  // }
+  if (msg.content.startsWith(PREFIX)) {
+    let [cmdName, ...args] = msg.content.trim().split(/ +/);
+    cmdName = cmdName.slice(PREFIX.length).toLowerCase();
+    let cmdObj = commands[cmdName];
+    console.log(cmdName);
+    if (cmdObj) {
+      cmdObj.action(msg, args);
+      console.log("sendingCommand");
     }
-    if (msg.content.toLowerCase().includes("ping")) {
-        msg.reply("pong");
-    }
-    if (msg.content.toLowerCase().includes("fix")) {
-        msg.reply("ur mom")
-        //msg.reply(`<@${msg.author.id}>`)
-    }
-    
-})
+  }
+  if (msg.content.toLowerCase().includes("ping")) {
+    msg.channel.send("pong");
+  }
+  if (msg.content.toLowerCase().includes("fix")) {
+    msg.channel.send("ur mom");
+    //msg.reply(`<@${msg.author.id}>`)
+  }
+});
 
-client.login(process.env.TOKEN)
- 
+client.login(process.env.TOKEN);
+
 // nossir
